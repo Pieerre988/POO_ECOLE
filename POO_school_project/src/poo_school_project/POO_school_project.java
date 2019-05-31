dd86b494e0de576c47154072952153201a61f371
Pieerre988 <pphilbert94@gmail.com>
2019-05-31 11:36:32 +0200

    beggin
    creation du dossier principal

---
+++ POO_school_project/src/poo_school_project/POO_school_project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo_school_project;
+
+/**
+ *
+ * @author Pierre
+ */
+public class POO_school_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
