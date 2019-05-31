8e5925cc80856d1761679426e2f8bbdc84e1831d
Pieerre988 <pphilbert94@gmail.com>
2019-05-31 11:45:47 +0200

    test Frame
    affichage d'un bouton tout simple

--- POO_school_project/src/poo_school_project/POO_school_project.java
+++ POO_school_project/src/poo_school_project/POO_school_project.java
@@ -16,6 +16,11 @@ public class POO_school_project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new connexion().setVisible(true);
+            }
+        });
     }
     
 }
