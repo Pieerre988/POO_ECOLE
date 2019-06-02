aa033eb7ee4fd7ebe934ae678094551f74d112f7
Pieerre988 <pphilbert94@gmail.com>
2019-06-03 00:29:01 +0200

    co
    ajout de la connexion

--- POO_school_project/src/poo_school_project/POO_school_project.java
+++ POO_school_project/src/poo_school_project/POO_school_project.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package poo_school_project;
-
+import connexion.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 /**
  *
  * @author Pierre
@@ -14,7 +16,13 @@ public class POO_school_project {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ArrayList<String> alpha = null;
+        
+        Connexion testTaCo = new Connexion("ecole","root","");
+        alpha = testTaCo.remplirChampsTable("personne");
+        alpha.forEach((value) -> System.out.println(value));
+        
         java.awt.EventQueue.invokeLater(() -> {
             new connexion().setVisible(true);
         });
